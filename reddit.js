97cab9a93870f8c47e0a9f8f13abbbe3a4d1884c
ds902022 <33606227+ds902022@users.noreply.github.com>
2022-02-22 19:52:12 +0000

    add role verification to musicbot

--- reddit.js
+++ reddit.js
@@ -2,6 +2,15 @@ const axios = require("axios");
 
 module.exports = {
     loadPage: (sub, message) => {
+        if (
+            !message.member.roles.cache.has("379325076740374528") &&
+            !message.member.roles.cache.has("279437266491801602") &&
+            !message.member.roles.cache.has("945331443435982868")
+        ) {
+            return message.channel.send(
+                "You do not have permission to use this command!"
+            );
+        }
         axios
             .get(
                 `https://www.reddit.com/r/${sub}.json?limit=100&?sort=top&t=all`
