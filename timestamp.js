b4255c50940dbd4a7910cc6ec3d1ee5048cc9195
ds902022 <33606227+ds902022@users.noreply.github.com>
2022-02-25 21:42:27 +0000

    revamp time, need to fix timezone

--- timestamp.js
+++ timestamp.js
@@ -1,10 +1,8 @@
 const timezones = require("./timezones.json");
 const Discord = require("discord.js");
+const { DateTime } = require("luxon");
 
-let targetChannel = null;
 const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-let errored = false;
-let offset = 0;
 
 const embedMessage = new Discord.MessageEmbed()
     .setColor("#00FFFF")
@@ -13,119 +11,112 @@ const embedMessage = new Discord.MessageEmbed()
 
 module.exports = {
     generateTimestamp: (channel, words) => {
-        targetChannel = channel;
         errored = false;
         offset = 0;
         if (words[1] === undefined || words[1] === "help") {
-            targetChannel.send(
+            channel.send(
                 "Valid inputs: \nTime in form: `hh:mm`\nDate in form: `dd/mm` or `dd/mm/yyyy`\nOptional timezone specifier: `UTC{+/-}hh` or abbreviation"
             );
             return;
         }
-        let date = new Date();
-        date.setHours(0);
-        date.setMinutes(0);
-        date.setSeconds(0);
-
+        words.shift();
+        let date = DateTime.utc();
+        date = date.set({ hour: 0, minute: 0, second: 0 });
         for (let word of words) {
-            dateModifiers.forEach((mod) => mod(word, date));
+            let success = false;
+            dateModifiers.forEach((mod) => {
+                vals = mod(word, date, success);
+                date = vals[0];
+                success = vals[1];
+            });
+            if (!success) {
+                channel.send(
+                    "Not following valid formats: \nTime in form: `hh:mm`\nDate in form: `dd/mm` or `dd/mm/yyyy`\nOptional timezone specifier: `UTC{+/-}hh` or abbreviation"
+                );
+                return;
+            }
         }
-        if (errored) {
-            error();
-            return;
-        }
-        let unixTime = parseInt(date.getTime() / 1000) + offset;
+        let unixTime = parseInt(date.toSeconds());
         embedMessage.setDescription(`<t:${unixTime}:F>`);
         embedMessage.fields = [];
         embedMessage.addFields({
             name: `Copy Link:`,
             value: `\\<t:${unixTime}:F>`,
         });
-        targetChannel.send(embedMessage);
+        channel.send(embedMessage);
     },
 };
 
-function error() {
-    targetChannel.send(
-        "Not following valid formats: \nTime in form: `hh:mm`\nDate in form: `dd/mm` or `dd/mm/yyyy`\nOptional timezone specifier: `UTC{+/-}hh` or abbreviation"
-    );
-}
-
 const dateModifiers = [parseDate, parseTime, setTimezone];
 
-function setTimezone(word, date) {
+function setTimezone(word, date, success) {
     zonesRegex = /^([a-z]+)$/;
     const zoneMatches = zonesRegex.exec(word);
     if (zoneMatches !== null) {
         const zone = zoneMatch(zoneMatches[1]);
         if (zone !== null) {
-            offset = -zone * 60 * 60;
-            return;
+            success = true;
+            date = date.setZone(zone, opts.keepLocalTime);
+            return [date, success];
         }
     }
-    timezoneRegex = /^(utc)?([+-]{1}[0-9]{1,2})$/;
+    timezoneRegex = /^(utc[+-]{1}[0-9]{1,2})$/;
     if (word.indexOf("+") === -1 && word.indexOf("-") === -1) {
-        return;
+        return [date, success];
     }
     const matches = timezoneRegex.exec(word);
     if (matches === null) {
-        errored = true;
-        return; // error does not match
+        return [date, success]; // error does not match
     }
-    const zone = matches[2];
-    offset = -zone * 60 * 60;
-    return;
+    const zone = matches[1];
+    success = true;
+    date = date.setZone(zone, opts.keepLocalTime);
+    return [date, success];
 }
 
-function parseTime(word, date) {
+function parseTime(word, date, success) {
     timeRegex = /^([0-9]{1,2}):([0-9]{2})$/;
     if (word.indexOf(":") === -1) {
-        return;
+        return [date, success];
     }
     const matches = timeRegex.exec(word);
     if (matches === null) {
-        errored = true;
-        return; // error does not match
+        return [date, success]; // error does not match
     }
-    const hour = matches[1];
-    const minute = matches[2];
-    if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
-        date.setHours(hour);
-        date.setMinutes(minute);
-    } else {
-        errored = true;
-        return;
+    const hours = matches[1];
+    const minutes = matches[2];
+    if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60) {
+        success = true;
+        date = date.set({ hour: hours, minute: minutes });
     }
+    return [date, success];
 }
 
-function parseDate(word, date) {
+function parseDate(word, date, success) {
     dateRegex = /^([0-9]{1,2})\/([0-9]{1,2})\/?([0-9]{4})?$/;
     if (word.indexOf("/") === -1) {
-        return;
+        return [date, success];
     }
     const matches = dateRegex.exec(word);
     if (matches === null) {
-        errored = true;
-        return; // error does not match
+        return [date, success]; // error does not match
     }
     const day = matches[1];
     const month = matches[2];
-    let year = matches[3];
-    if (year !== undefined) {
-        date.setYear(year);
-    }
+    const year = matches[3];
     if (
         day >= 1 &&
         day <= monthLength(month, year) &&
         month >= 1 &&
         month <= 12
     ) {
-        date.setDate(day);
-        date.setMonth(month - 1);
-    } else {
-        errored = true;
-        return;
+        if (year !== undefined) {
+            date = date.set({ year: year });
+        }
+        success = true;
+        date = date.set({ day: day, month: month });
     }
+    return [date, success];
 }
 
 function zoneMatch(zone) {
