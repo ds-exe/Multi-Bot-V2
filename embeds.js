3ff46ad6b74f7be9bdf16dbd7d3ba649ec6e16fd
ds902022 <33606227+ds902022@users.noreply.github.com>
2022-04-23 01:11:17 +0100

    remove large image from help

--- embeds.js
+++ embeds.js
@@ -3,13 +3,12 @@ const Discord = require("discord.js");
 exports.helpEmbed = new Discord.MessageEmbed()
     .setColor("#0099ff")
     .setTitle("🍑 UwU Bot")
-    .setDescription("Be Gentle Senpai")
-    .setThumbnail("https://i.imgur.com/RwkB33I.png")
+    .setDescription("Use Me Senpai")
+    .setThumbnail("https://i.imgur.com/twtmzmr.png")
     .addFields({
         name: "UwU Commands Available Here 🔎",
         value: "```📌!time or !date\n📌!timezone\n📌!reddit\n📌!play\n📌!skip\n📌!stop\n📌!leave\n📌!perms```",
     })
-    .setImage("https://i.imgur.com/twtmzmr.png")
     .setFooter(
         "BOT made by @ds#8460",
         "https://cdn.discordapp.com/avatars/74968333413257216/3bf0047dd6175e5b623ce6d5ade1a76e.webp"
